9c53c5c10da7f1d3d0c7b84b20e736ceddadc156
mikeamoh <108932914+mikeamoh@users.noreply.github.com>
2022-07-08 08:20:56 -0400

    Create mike.sh

---
+++ mike.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo "what is your name?"
+read f_name
+
+
+echo "what stae are you from?"
+read state
+
+echo "how many kids do ou have?"
+read kids
+
+
+#echo "your name is ${f_name} , you live in ${state} and have ${kids}"
+if
+ [ ${kids} -lt 2 ]
+then
+echo "3 more to go"
+else
+echo "thats enough"
+fi
